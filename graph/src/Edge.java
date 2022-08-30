1b44412c91fd893c9d8da268e9b9f8a2ad12b540
Freyness <dinugabriel.v.2001@gmail.com>
2022-08-30 12:42:09 +0400

    Structure of Classes: - Edge - Street - Vertex
    And Interface WAY

--- graph/src/Edge.java
+++ graph/src/Edge.java
@@ -1,7 +1,44 @@
-public class Edge {
+import codedraw.CodeDraw;
 
-    public Edge(){
+import static java.lang.Math.*;
+
+public class Edge /*extends Street*/ implements Way {
+
+    private final String streetName;
+    private final Vertex v1, v2;
+
+    public Edge(Vertex v1, Vertex v2){
+        //super( "","", "");
+        this.streetName = "";//super.getName();
+        this.v1 = v1;
+        this.v2 = v2;
+
     }
+
+    public Vertex getV1(){ return v1; }
+    public Vertex getV2(){ return v2; }
+
+    @Override
+    public double getLength(){
+        return sqrt((pow(v2.getLat() - v1.getLat(), 2)) + pow(v2.getLon() - v1.getLon(), 2));
+    }
+
+    @Override
+    public String getName() {
+        return streetName;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder s = new StringBuilder();
+        s.append("EDGE OF STREET ").append(this.streetName).append("\n");
+        s.append("WITH THE NODES: \n");
+        s.append(this.v1).append("\n");
+        s.append(this.v2).append("\n");
+
+        return s.toString();
+    }
+
 
 
 }
--- graph/src/Street.java
+++ graph/src/Street.java
@@ -1,2 +1,52 @@
-public class Street {
+import codedraw.CodeDraw;
+
+import java.util.ArrayList;
+
+public class Street implements Way{
+
+    private ArrayList<Edge> edges = new ArrayList<>();
+    private double length = 0.0;
+    private final String id, name, type;
+
+    public Street(String id, String name, String type){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public Street(String id, String name,String type, ArrayList<Edge> e){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        for(Edge i : e) this.addEdge(i);
+    }
+
+    public void addEdge(Edge e){
+        edges.add(e);
+        length += e.getLength();
+    }
+
+    public ArrayList<Edge> getEdges(){ return this.edges; }
+
+    @Override
+    public double getLength() {
+        return length;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder b = new StringBuilder("STREET - " + name + " - " + id);
+        b.append("\n");
+        b.append(edges);
+        return b.toString();
+    }
+
+
+
+
 }
