fdfd44635d80f9b5e27536463b397af9d4eb44b3
Freyness <dinugabriel.v.2001@gmail.com>
2022-09-01 15:08:50 +0400

    Added Street Merger to OSMParser Changed the return type of getStreets to HashMap<String, Street>
    Updated Main

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.io.FileNotFoundException;
 import java.util.*;
 import codedraw.*;
-
+import java.time.LocalTime;
 public class main {
 
     public static OSM osm = OSM.getInstance("data/map.osm");
@@ -14,7 +14,9 @@ public class main {
 
     public static void main(String[] args) throws XMLStreamException, FileNotFoundException {
 
-        ArrayList<Street> streets = parser.getStreets(osm.getVertex());
+        LocalTime start = LocalTime.now();
+
+        streets.putAll(parser.getStreets(osm.getVertex()));
         System.out.println("NR STREETS: " + streets.size());
 
 //        for(Street i : streets){
@@ -24,47 +26,57 @@ public class main {
 //            }
 //        }
 
-        for(Street i : streets){
+        for(Street i : streets.values()){
             switch(i.getType()){
                 case "highway-primary":
                     GUI.setColour(Color.red);
-                    GUI.setLineWidth(7);
+                    GUI.setLineWidth(4);
                     GUI.drawStreet(i);
                     break;
                 case "highway-secondary":
                     GUI.setColour(Color.orange);
-                    GUI.setLineWidth(5);
+                    GUI.setLineWidth(3);
                     GUI.drawStreet(i);
                     break;
                 case "highway-tertiary":
                     GUI.setColour(Color.GREEN);
-                    GUI.setLineWidth(5);
+                    GUI.setLineWidth(3);
                     GUI.drawStreet(i);
                     break;
                 case "highway-residential":
                     GUI.setColour(Color.blue);
-                    GUI.setLineWidth(3);
+                    GUI.setLineWidth(2);
                     GUI.drawStreet(i);
                     break;
                 case "highway-pedestrian":
                 case "highway-footway":
                     GUI.setColour(Color.MAGENTA);
-                    GUI.setLineWidth(3);
+                    GUI.setLineWidth(1);
                     GUI.drawStreet(i);
                     break;
 
-                default:
-                    if(i.getType().contains("highway-")){
-                        GUI.setColour(Color.darkGray);
-                        GUI.setLineWidth(3);
-                        GUI.drawStreet(i);
-
-                    }
+//                default:
+//                    if(i.getType().contains("highway-")){
+//                        GUI.setColour(Color.darkGray);
+//                        GUI.setLineWidth(1);
+//                        GUI.drawStreet(i);
+//
+//                    }
             }
-            GUI.show();
+
+            for(Edge e : streets.get("Franz-Josefs-Kai").getEdges()){
+                GUI.setColour(Color.cyan);
+                GUI.setLineWidth(3);
+                GUI.drawEdge(e);
+            }
+
         }
 
-        System.out.println("FINISHED DRAWING");
+        GUI.show();
+
+        //System.out.println("FINISHED DRAWING");
+
+        //System.out.println("Total Run Time: " + (LocalTime.now().getSecond() - start.getSecond()));
     }
 
 }
